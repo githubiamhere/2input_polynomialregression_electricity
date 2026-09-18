dc369f57d0f4544942c565eefa31e982e5e6facb
Michel <149605902+githubiamhere@users.noreply.github.com>
2026-09-18 11:34:07 +0530

    Update 2input_electricity_bill_app.py

--- 2input_electricity_bill_app.py
+++ 2input_electricity_bill_app.py
@@ -2,23 +2,46 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("2input_electricity_bill_prediction_model.pkl")
+model = joblib.load("2input_electricity_bill_perdiction_model.pkl")
 poly = joblib.load("poly.pkl")
+
 st.title("Electrical_Bill AC Price Prediction")
 
-units = st.number_input(
+ac_units = st.number_input(
     "Enter AC Units : ",
-    min_value=0.0,
-    max_value=150.0,
+    min_value=1.0,
     value=100.0
 )
 
+fan_units = st.number_input(
+    "Enter Fan Units : ",
+    min_value=1.0,
+    value=100.0
+)
 
 if st.button("Predict"):
+
+    valid = True
+
+    if ac_units <= 0 or ac_units > 150:
+        st.error("AC Units should be between 0 and 150")
+        valid = False
+
+    if fan_units <= 0 or fan_units > 150:
+        st.error("Fan Units should be between 0 and 150")
+        valid = False
+
+    if valid:
+
         input_data = pd.DataFrame({
-            "AC_Units": [units],
+            "AC_Units": [ac_units],
+            "Fan_Units": [fan_units]
         })
+
         input_data_poly = poly.transform(input_data)
+
         prediction = model.predict(input_data_poly)
+
         pred = prediction[0]
-        st.success(f"Predicted Price: ₹{pred:.2f} Rs")
+
+        st.success(f"Predicted Price: ₹{pred:.2f}")
